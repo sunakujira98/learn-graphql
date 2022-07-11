fd25ebcfc4bc0c8ced74d06732e3dd649c65d298
Steven Theodore <steventheodore98@gmail.com>
2022-07-11 23:46:55 +0700

    Add project mutation

--- client/src/App.js
+++ client/src/App.js
@@ -37,7 +37,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/project/:id' element={<Project />} />
+            <Route path='/projects/:id' element={<Project />} />
             <Route path='*' element={<NotFound />} />
           </Routes>
         </div>
